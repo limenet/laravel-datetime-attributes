a975a3fab9368cc0f9c0b0383780327e5d066e46
Linus Metzler <hi@linusmetzler.me>
2025-02-22 09:04:41 +0100

    rector

--- rector.php
+++ rector.php
@@ -9,8 +9,4 @@ return RectorConfig::configure()
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withPhpSets();
